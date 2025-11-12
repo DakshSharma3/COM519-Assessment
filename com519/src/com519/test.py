0b92f16b03a3bf9e0c11ca4da53e5d79415bdbd0
Daksh Sharma <0shard85@solent.ac.uk>
2025-11-12 12:58:32 +0000

    Testing out xml

--- com519/src/com519/test.py
+++ com519/src/com519/test.py
@@ -1,22 +1,47 @@
-# def valid_check(text, function):
-#     for character in text:
-#         if function(character):
-#             return True
-#     return False
+# import xml.etree.cElementTree as ET
 #
-# string = "abcd123"
+# root = ET.Element("root")
 #
-# print(valid_check(string,str.isdigit))
-from com519.src.com519.database import Database
+#
+# credentials =[["admin","password"], ["admin1","password1"], ["admin2","password2"] ]
+#
+#
+# for credential in credentials:
+#
+#     sub_element = ET.SubElement(root, "credentials")
+#
+#     ET.SubElement(sub_element, "Username").text = credential[0]
+#     ET.SubElement(sub_element, "Password").text = credential[1]
+#
+# ET.indent(root, space="    ")
+# tree = ET.ElementTree(root)
+# tree.write("filename.xml")
 
-postcode = "so31 5gg"
-address = "8 Cranmore"
-db = Database("COM519.db")
-# postcode = postcode.upper().replace(" ", "")
-# query = """SELECT * FROM Address WHERE Postcode = ?;"""
-# results = db.cursor.execute(query, (postcode,)).fetchall()
-# print(results)
 
-query = """INSERT INTO Address (Address, Postcode) VALUES (?, ?);"""
-db.cursor.execute(query, (address, postcode))
-db.connection.commit()
+# importing element tree
+# under the alias of ET
+import xml.etree.ElementTree as ET
+from fileinput import filename
+
+# Passing the path of the
+# xml document to enable the
+# parsing process
+tree = ET.parse('filename.xml')
+
+# getting the parent tag of
+# the xml document
+root = tree.getroot()
+
+# printing the root (parent) tag
+# of the xml document, along with
+# its memory location
+print(root)
+
+# printing the attributes of the
+# first tag from the parent
+print(root[0][0])
+
+# printing the text contained within
+# first subtag of the 5th tag from
+# the parent
+print(root[0][0].text)
