f048df2f2289db85963ba15f1cb55efdf7f4199c
Daksh Sharma <daksh.sharma.ds11@gmail.com>
2025-11-23 21:07:20 +0000

    Alligning main menu text

--- com519/src/com519/main_menu.py
+++ com519/src/com519/main_menu.py
@@ -14,18 +14,14 @@ class MainMenu(tk.Tk):
         self.title("Main Menu")
         self.geometry("300x300")
         self.db = Database(self.database_name)
+
+        for i in range (0,3):
+            self.rowconfigure(i, weight=1)
         for i in range (0,2):
             self.columnconfigure(i, weight=1)
-        self.rowconfigure(0, weight=2)
-        for i in range (1,3):
-            self.rowconfigure(i, weight=1)
-        for r in range(3):
-            for c in range(2):
-                frame = tk.Frame(self, bd=1, relief="solid")  # outline
-                frame.grid(row=r, column=c, sticky="nsew")
 
-        username_text = tk.Label(self, text="Main Menu", font=("Arial", 16))
-        username_text.grid(column=1, row=0, padx=10, pady=10, columnspan=2)
+        main_menu_text = tk.Label(self, text="Main Menu", font=("Arial", 16))
+        main_menu_text.grid(column=0, row=0, padx=10, pady=10, columnspan=2)
 
         add_account_button = tk.Button(self, text="Add Account")
         add_account_button.grid(column=0, row=1, padx=10, pady=10)
@@ -40,8 +36,8 @@ class MainMenu(tk.Tk):
         view_appointment_button.grid(column=1, row=2, padx=10, pady=10)
 
     def on_closing(self):
-        if messagebox.askokcancel("Quit", "Do you want to quit?"):
-            self.destroy()
+        # if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        self.destroy()
 
 main = MainMenu()
 main.protocol("WM_DELETE_WINDOW", main.on_closing)
