15e1febad04001a44c6a03b4588720ff18014747
Daksh Sharma <0shard85@solent.ac.uk>
2025-12-08 23:53:05 +0000

    Working on view accounts page

--- com519/src/com519/view_account.py
+++ com519/src/com519/view_account.py
@@ -12,9 +12,11 @@ class ViewAccount(tk.Toplevel):
         self.user = user
         self.database_name = "COM519.db"
         self.title("My Tkinter App")
-        self.geometry("350x400")
+        self.geometry("450x400")
         self.account_type = tk.StringVar()
         self.db = Database(self.database_name)
+        self.accounts = self.db.get_all_user_bank_accounts(self.user.get_people_id())
+        self.selected_account = None
 
         for i in range (0,3):
             self.rowconfigure(i, weight=1)
@@ -35,22 +37,38 @@ class ViewAccount(tk.Toplevel):
         self.available_account_listbox.bind('<<ListboxSelect>>', self.action)
 
         self.available_account_listbox.grid(column=0, row=1, padx=10)
-        for account in self.db.get_all_user_bank_accounts(self.user.get_people_id()):
-            self.available_account_listbox.insert(tk.END, account[1])
+        print(self.accounts[1])
+        for account in self.accounts:
+            self.available_account_listbox.insert(tk.END, account)
 
-        # specific_account_listbox = tk.Listbox(self, height=10,
-        #                      width=15,
-        #                      bg="grey",
-        #                      activestyle='dotbox',
-        #                      font="Helvetica",
-        #                      fg="yellow")
+        self.specific_account_listbox = tk.Listbox(self, height=10,
+                             width=22,
+                             bg="white",
+                             activestyle='dotbox',
+                             font="Helvetica",
+                             fg="yellow",
+                             selectmode="browse")
+        self.specific_account_listbox.grid(column=1, row=1, padx=10)
 
         register_button = tk.Button(self, text="Register Account")
         register_button.grid(column=0, row=2, padx=10, pady=10, columnspan=2)
 
 
     def action(self, event):
-        print("Account Type is " , self.available_account_listbox.get(self.available_account_listbox.curselection()))
+        # print("Account Type is " , self.available_account_listbox.get(self.available_account_listbox.curselection()))
+        print("Account Type is ", self.available_account_listbox.curselection()[0])
+        self.selected_account = self.available_account_listbox.get(self.available_account_listbox.curselection()[0])
+        self.fill_selected_account_list()
+
+
+    def fill_selected_account_list(self):
+        self.specific_account_listbox.config(state="normal")
+        self.specific_account_listbox.delete(0, tk.END)
+        self.specific_account_listbox.insert(tk.END, f"Account Type: {self.selected_account[1]}")
+        self.specific_account_listbox.insert(tk.END, "")
+        self.specific_account_listbox.insert(tk.END, f"Account Balance: {self.selected_account[2]}")
+        self.specific_account_listbox.config(state="disabled")
+
 
     def on_closing(self):
         """Closes the application"""
